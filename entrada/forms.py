63745db728cebd46da701824d5c14c0c4478640d
Kauê Fernandes <153867670+kaue-fernandescp@users.noreply.github.com>
2024-10-09 15:44:17 -0300

    Delete entrada directory

--- entrada/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Entradas
-
-class EntradaForm(ModelForm):
-    class Meta:
-        model = Entradas
-        fields = ['produto', 'quantidade', 'preco']
